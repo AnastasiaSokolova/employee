d3acb73918eadd2568c1cee60f470304acc8abea
Nastya Sokolova <nas92sokol@gmail.com>
2016-09-06 16:51:13 +0300

    prod and test config

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,3 +1,37 @@
-/**
- * Created by Настя on 05.09.2016.
- */
+var webpackConfig = require('./webpack.test');
+
+module.exports = function (config) {
+    var _config = {
+        basePath: '',
+
+        frameworks: ['jasmine'],
+
+        files: [
+            {pattern: './config/karma-test-shim.js', watched: false}
+        ],
+
+        preprocessors: {
+            './config/karma-test-shim.js': ['webpack', 'sourcemap']
+        },
+
+        webpack: webpackConfig,
+
+        webpackMiddleware: {
+            stats: 'errors-only'
+        },
+
+        webpackServer: {
+            noInfo: true
+        },
+
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: ['PhantomJS'],
+        singleRun: true
+    };
+
+    config.set(_config);
+};
