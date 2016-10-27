3bd752f4b0f37f6b3996c63472b817104a78eb40
unknown <Anastasiia_Sokolova@EPUALVIW1140.kyiv.epam.com>
2016-10-27 11:58:32 +0300

    edit

--- src/app/shared/employees-resolve.service.ts
+++ src/app/shared/employees-resolve.service.ts
@@ -12,15 +12,12 @@ export class EmployeesResolve implements Resolve<Employees> {
   constructor( private es: EmployeesService,
               private router: Router ) {}
 
-  resolve( route: ActivatedRouteSnapshot ): Promise<boolean>|boolean {
+  resolve( route: ActivatedRouteSnapshot ): Promise<Employees>|boolean {
     let id = route.params['id'];
     return this.es.getEmployee(id).then(res => {
-      if (res) {
         return res;
-      } else { 
-        this.router.navigate(['/employees']);
-        return false;
-      }
+    }, err => {
+      this.router.navigate(['/employees']);
     });
   }
 }
